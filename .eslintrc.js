f22188591ed97cfbd13fb2f66a881177bdf60c12
SeWonKim <swon962@gmail.com>
2022-01-16 01:49:06 +0900

    refactor: 💡 tab width 수정
    1. useTabs를 false로 설정\n2. eslint에서 indent 설정 off\n3. vsc 껐다켜기

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,48 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
-		'airbnb',
-		'prettier',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-			tsx: true,
-		},
-		ecmaVersion: 13,
-		sourceType: 'module',
-	},
-	plugins: ['react', '@typescript-eslint', 'prettier'],
-	rules: {
-		indent: [2, 'tab'],
-		'react/jsx-indent': [2, 'tab'],
-		'react/jsx-indent-props': [2, 'tab'],
-		'no-tabs': 0,
-		'react/jsx-filename-extension': [
-			2,
-			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-		],
-		'react/jsx-one-expression-per-line': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'import/extensions': [
-			'error',
-			'ignorePackages',
-			{
-				js: 'never',
-				jsx: 'never',
-				ts: 'never',
-				tsx: 'never',
-			},
-		],
-		'prettier/prettier': ['error', { endOfLine: 'auto' }],
-	},
-	settings: {
-		'import/resolver': {
-			node: {
-				extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			},
-		},
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
