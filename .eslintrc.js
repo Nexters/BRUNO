4017b949d63d1f153fc6675e3f6a2e63f40ec696
sbyeol3 <dev.sbyeol@gmail.com>
2022-01-16 00:50:50 +0900

    chore: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+			tsx: true,
+		},
+		ecmaVersion: 13,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: [2, 'tab'],
+		'react/jsx-indent': [2, 'tab'],
+		'react/jsx-indent-props': [2, 'tab'],
+		'no-tabs': 0,
+		'react/jsx-filename-extension': [
+			2,
+			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+		],
+		'react/jsx-one-expression-per-line': 'off',
+		'react/react-in-jsx-scope': 'off',
+	},
+};
