ed08fba68f7ed51b2262bd43fe65ff9e9fd837f7
sbyeol3 <dev.sbyeol@gmail.com>
2022-01-22 00:31:51 +0900

    style: add temp colors

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -2,7 +2,16 @@ import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
   colors: {
-    main: 'gold',
-    secondary: 'silver',
+    gray00: '#000000',
+    gray10: '#292A2C',
+    gray20: '#333436',
+    gray30: '#3E4043',
+    gray40: '#5B5D61',
+    gray50: '#9B9EA5',
+    gray60: '#C8CACF',
+    gray70: '#D7D9DD',
+    gray80: '#E4E5E9',
+    gray90: '#F2F2F4',
+    gray100: '#FFFFFF',
   },
 };
