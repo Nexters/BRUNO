fc5f0dc9625fb2582b4c08898adbc946577e1bbe
sbyeol3 <dev.sbyeol@gmail.com>
2022-01-30 01:40:52 +0900

    feat: add KlipLoginPage

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
-import OtherWalletPage from '@src/pages/OtherWalletPage';
 import LoginPage from '@src/pages/LoginPage';
+import KlipLoginPage from '@src/pages/KlipLoginPage';
+import OtherWalletPage from '@src/pages/OtherWalletPage';
 
 function Auth() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
+      <Route path="/klip" element={<KlipLoginPage />} />
       <Route path="/other" element={<OtherWalletPage />} />
     </Routes>
   );
