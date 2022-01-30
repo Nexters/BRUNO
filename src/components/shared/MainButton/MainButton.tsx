f0dc720d8159343b89dfd8f4755c69792759a778
SeWonKim <swon962@gmail.com>
2022-01-30 12:12:14 +0900

    style: 💄 theme에 font-size 추가

--- src/components/shared/MainButton/MainButton.tsx
+++ src/components/shared/MainButton/MainButton.tsx
@@ -16,6 +16,7 @@ const Button = styled.button<{ buttonStyle?: string }>`
   padding: 13px;
   border-radius: 44px;
   background: ${(props) => props.theme.background.button00};
+  font-size: ${(props) => props.theme.fontSize.body02};
   font-weight: bold;
   color: ${(props) => props.theme.colors.gray100};
   ${(props) => props.buttonStyle};
