cedbb47e34c93522dd5275a18d4ef7b2dbb2f5bc
SeWonKim <swon962@gmail.com>
2022-01-30 15:32:16 +0900

    style: 💄 svg Icon 추가

---
+++ src/assets/Icon/index.ts
@@ -0,0 +1,5 @@
+export { default } from './Icon';
+export { ReactComponent as Profile24 } from './24px-profile.svg';
+export { ReactComponent as Home24 } from './24px-home.svg';
+export { ReactComponent as AlarmOn24 } from './24px-alarm-on.svg';
+export { ReactComponent as Setting24 } from './24px-setting.svg';
