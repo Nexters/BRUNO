2326a430c753f0d8baf4680d146a2c791f97e5a6
SeWonKim <swon962@gmail.com>
2022-01-30 13:21:32 +0900

    feat: 🎸 Main router - Home page 추가

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import PageLayout from '@src/components/shared/PageLayout';
+
+function HomePage() {
+  return (
+    <PageLayout padding="20px">
+      <p style={{ color: 'white' }}>HOME</p>
+    </PageLayout>
+  );
+}
+
+export default HomePage;
