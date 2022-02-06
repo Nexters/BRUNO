5bb6dafd592414004a31d33ed0bf5c760908cc6f
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-06 18:28:43 +0900

    feat: add selected style in HomeTab category btn

--- src/components/HomeTab/HomeTab.tsx
+++ src/components/HomeTab/HomeTab.tsx
@@ -20,7 +20,8 @@ export default function HomeTab() {
           // eslint-disable-next-line react/no-array-index-key
           key={`CATEGORY_${index}`}
           category={category}
-          color={COLORS[index % COLORS.length]}
+          color={COLORS[(index + 1) % COLORS.length]}
+          isSelected={index === 0} // TODO : selected 로직 추가
         />
       ))}
     </Wrapper>
--- src/components/shared/CategoryButton/CategoryButton.tsx
+++ src/components/shared/CategoryButton/CategoryButton.tsx
@@ -1,18 +1,21 @@
 import styled from 'styled-components';
+import { rgba } from 'polished';
 
 type Props = {
   category: string;
   color: string;
+  isSelected: boolean;
 };
 
-const Button = styled.button`
+const Button = styled.button<{ isSelected: boolean; color: string }>`
   width: fit-content;
   height: 40px;
   border: 1px solid ${(props) => props.color};
   padding: 7px 16px;
   border: 1px solid ${(props) => props.color};
   border-radius: 37px;
-  background-color: transparent;
+  background-color: ${({ isSelected, color }) =>
+    isSelected ? rgba(color, 0.5) : 'transparent'};
   color: ${(props) => props.theme.colors.gray100};
   font-weight: bold;
   font-size: ${(props) => props.theme.fontSize.body02};
@@ -20,6 +23,10 @@ const Button = styled.button`
   white-space: nowrap;
 `;
 
-export default function CategoryButton({ category, color }: Props) {
-  return <Button color={color}>{category}</Button>;
+export default function CategoryButton({ category, color, isSelected }: Props) {
+  return (
+    <Button color={color} isSelected={isSelected}>
+      {category}
+    </Button>
+  );
 }
