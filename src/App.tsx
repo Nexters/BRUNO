4ae561a5c22338bbfc9af3335f9e75accd612d98
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-08 01:34:48 +0900

    build: add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import Main from '@src/routes/Main';
 import Auth from '@src/routes/Auth';
 import User from '@src/routes/User';
 import CreateCookiePage from '@src/pages/CreateCookiePage';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login/*" element={<Auth />} />
-          <Route path="/user/*" element={<User />} />
-          <Route path="/create/cookie/" element={<CreateCookiePage />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login/*" element={<Auth />} />
+            <Route path="/user/*" element={<User />} />
+            <Route path="/create/cookie/" element={<CreateCookiePage />} />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+      {/* dev tool 제거 시 아래 line comment out! */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
