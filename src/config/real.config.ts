e3554b5b132c21ecbbc7ae3c02e59ddbd5b965c7
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-18 23:36:54 +0900

    build: add abi and klip-sdk

--- src/config/real.config.ts
+++ src/config/real.config.ts
@@ -2,4 +2,5 @@ export default {
   chainId: 8217, // cypress
   accessKeyId: 'KASK7EHHAP596P4OE3PF72CS',
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  baseApiUrl: 'http://api.cookiepang.site',
 };
