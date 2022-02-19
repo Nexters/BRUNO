00043da865adf5d209d34a449c237abaccd35e81
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-20 01:59:26 +0900

    feat: add getCookie query

--- src/queries/cookies.ts
+++ src/queries/cookies.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import config from '@src/config';
-import { useInfiniteQuery } from 'react-query';
+import { useQuery } from 'react-query';
+import { CommonUseQuery, CookieType } from './types';
 
 export const getCookieList = (page?: number) =>
   axios.get(`${config.baseApiUrl}/categories/all/cookies`, {
     params: {
       page: page ?? 0,
-      size: 5,
+      size: 10,
     },
     headers: {
       'Content-Type': 'application/json',
@@ -24,13 +25,10 @@ export const postCookie = ({ title, contents, hammer, txHash }: any) =>
     categoryId: 1,
   });
 
-export const getCookieListByCategory = async (
-  categoryId: string,
-  page: number,
-) =>
+export const getCookieListByCategory = async (categoryId: string, page: number) =>
   axios.get(`${config.baseApiUrl}/categories/${categoryId}/cookies`, {
     params: {
-      page,
+      page: page ?? 0,
       size: 5,
     },
     headers: {
@@ -38,9 +36,12 @@ export const getCookieListByCategory = async (
     },
   });
 
-export const useGetAllCookies = () => {
-  const { data } = useInfiniteQuery(
-    ['categories', 'all', 'cookies'],
-    ({ pageParam }) => getCookieList(pageParam),
-  );
+type UseGetAllCookie = CommonUseQuery & {
+  cookieList: CookieType[];
+};
+
+export const useGetAllCookies = (): UseGetAllCookie => {
+  const { data, isLoading, isError } = useQuery(['categories', 'all', 'cookies'], () => getCookieList(0));
+
+  return { cookieList: data?.data ?? [], isLoading, isError };
 };
