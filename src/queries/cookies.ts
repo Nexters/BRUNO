d99dc6baa1b43f5c36deadf74f4c6fa5aba6f02c
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-19 17:52:50 +0900

    feat: add queries and config

---
+++ src/queries/cookies.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import config from '@src/config';
+import { useInfiniteQuery } from 'react-query';
+
+const getCookieList = (page: number) =>
+  axios.get(`${config.baseApiUrl}/categories/all/cookies`, {
+    params: {
+      page,
+      size: 5,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+export const postCookie = ({ title, contents, hammer, txHash }: any) =>
+  axios.post(`${config.baseApiUrl}/cookies`, {
+    question: title,
+    answer: contents,
+    price: hammer,
+    authorUserId: 1,
+    ownedUserId: 1,
+    txHash,
+    categoryId: 1,
+  });
+
+export const getCookieListByCategory = async (
+  categoryId: string,
+  page: number,
+) =>
+  axios.get(`${config.baseApiUrl}/categories/${categoryId}/cookies`, {
+    params: {
+      page,
+      size: 5,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+export const useGetAllCookies = () => {
+  const { data } = useInfiniteQuery(
+    ['categories', 'all', 'cookies'],
+    ({ pageParam }) => getCookieList(pageParam),
+  );
+};
