664ae106a30e1ef36234a7cc02b98500f9874cf5
SeWonKim <swon962@gmail.com>
2022-02-19 21:58:43 +0900

    feat: 🎸 subHeader skip 버튼 추가

--- src/pages/TutorialPage/TutorialPage.tsx
+++ src/pages/TutorialPage/TutorialPage.tsx
@@ -1,5 +1,12 @@
+import { HeaderPage } from '@src/components/Header/const';
+import PageLayout from '@src/components/shared/PageLayout';
+
 function TutorialPage() {
-  return <div>TutorialPage</div>;
+  return (
+    <PageLayout padding="20px" pageType={HeaderPage.TUTORIAL} onlyContents>
+      Tutorial
+    </PageLayout>
+  );
 }
 
 export default TutorialPage;
