15b25a75f5683c519a165f22642509eea8608809
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-20 15:15:15 +0900

    refactor: add axios globl config

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
+import '@src/config/axios';
 import Main from '@src/routes/Main';
 import Auth from '@src/routes/Auth';
 import User from '@src/routes/User';
@@ -29,23 +30,11 @@ function App() {
             <BrowserRouter>
               <Routes>
                 {/* Auth */}
-                <Route
-                  path="/login"
-                  element={<Auth component={<LoginPage />} />}
-                />
-                <Route
-                  path="/join"
-                  element={<Auth component={<JoinPage />} />}
-                />
+                <Route path="/login" element={<Auth component={<LoginPage />} />} />
+                <Route path="/join" element={<Auth component={<JoinPage />} />} />
                 {/* Authentication이 필요한 경우 PrivateRoute로 감싸주어야 함 */}
-                <Route
-                  path="*"
-                  element={<PrivateRoute component={<Main />} />}
-                />
-                <Route
-                  path="/user/*"
-                  element={<PrivateRoute component={<User />} />}
-                />
+                <Route path="*" element={<PrivateRoute component={<Main />} />} />
+                <Route path="/user/*" element={<PrivateRoute component={<User />} />} />
               </Routes>
             </BrowserRouter>
           </Suspense>
--- src/queries/categories.ts
+++ src/queries/categories.ts
@@ -1,9 +1,3 @@
 import axios from 'axios';
-import config from '@src/config';
 
-export const getCategoryist = () =>
-  axios.get(`${config.baseApiUrl}/categories`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+export const getCategoryist = () => axios.get('/categories');
