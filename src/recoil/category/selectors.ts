dee470999f73b904d1f2c40729534cc2c9bebedd
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-20 21:21:55 +0900

    feat: categorySelector

--- src/recoil/category/selectors.ts
+++ src/recoil/category/selectors.ts
@@ -1,4 +1,4 @@
-import { selector } from 'recoil';
+import { selector, selectorFamily } from 'recoil';
 import { getCategoryist } from '@src/queries/categories';
 import { Category } from './types';
 
@@ -9,4 +9,15 @@ export const categoryListSelector = selector<Category[]>({
     if (categories) return categories;
     return [];
   },
+});
+
+// 카테고리 목록에서 특정 카테고리만 가져오는 셀렉터
+export const categorySelector = selectorFamily<Category | null, number>({
+  key: 'category/selector',
+  get:
+    (categoryId: number) =>
+    async ({ get }) => {
+      const categories = await get(categoryListSelector);
+      return categories.find((item) => item.categoryId === categoryId) || null;
+    },
 });
