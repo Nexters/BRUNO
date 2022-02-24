f863a09ab8faa49dc5d7b83efa38e6974bbe6880
SaetByeol Ahn <dev.sbyeol@gmail.com>
2022-02-25 01:38:44 +0900

    feat: add bg image in userhome (#53)

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -2,6 +2,8 @@ import MainLogo01 from './logo/Main_01.png';
 import ProfileImage01 from './profile/Default_01.png';
 import ProfileImage02 from './profile/Default_02.png';
 import ProfileImage03 from './profile/Default_03.png';
+import BackgroundImage01 from './profile/Default_bg_01.png';
+import BackgroundImage02 from './profile/Default_bg_02.png';
 import NFTImage01 from './nft/Default_01.png';
 import NFTImage02 from './nft/Default_02.png';
 import NFTImage03 from './nft/Default_03.png';
@@ -20,6 +22,8 @@ export {
   ProfileImage01,
   ProfileImage02,
   ProfileImage03,
+  BackgroundImage01,
+  BackgroundImage02,
   NFTImage01,
   NFTImage02,
   NFTImage03,
