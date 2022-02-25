cce80470214a5d2dbbfb238c70eaed2e430773d5
sbyeol3 <dev.sbyeol@gmail.com>
2022-02-26 02:21:49 +0900

    fix: fix wrong meta imageurls

--- src/klip/const.ts
+++ src/klip/const.ts
@@ -1,6 +1,6 @@
 export const COOKIE_IMAGE_URLS: { [key in string]: string } = {
-  PINK: 'https://cdn.cookiepang.site/metadata/cookie_01.json',
-  BLUE: 'https://cdn.cookiepang.site/metadata/cookie_02.json',
-  YELLOW: 'https://cdn.cookiepang.site/metadata/cookie_03.json',
-  PURPLE: 'https://cdn.cookiepang.site/metadata/cookie_04.json',
+  PINK: 'https://cdn.cookiepang.site/metadata/cookie_meta01.json',
+  BLUE: 'https://cdn.cookiepang.site/metadata/cookie_meta02.json',
+  YELLOW: 'https://cdn.cookiepang.site/metadata/cookie_meta03.json',
+  PURPLE: 'https://cdn.cookiepang.site/metadata/cookie_meta04.json',
 };
