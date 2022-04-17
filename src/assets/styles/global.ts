ca7ddb94f2d664e05c44f509c94b1b340c12bf03
SeWonKim <swon962@gmail.com>
2022-04-17 17:15:13 +0900

    fix: 🐛 mobile web height issue
    모바일 웹 address bar 때, 잘리는 문제 해결

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -10,6 +10,10 @@ export const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
 
   html,
   body {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    bottom: 0;
     overflow: hidden;
     background-color: ${(props) => props.theme.colors.basic.gray20};
     color: ${(props) => props.theme.colors.basic.gray100};
