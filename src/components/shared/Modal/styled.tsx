16abf763d0006058b16eb26442d9e3b13b545c3e
SeWonKim <swon962@gmail.com>
2022-04-17 17:15:13 +0900

    refactor: 💡 Modal component structure
    불필요한 div 제거

--- src/components/shared/Modal/styled.tsx
+++ src/components/shared/Modal/styled.tsx
@@ -3,21 +3,14 @@ import { rgba } from 'polished';
 import { Button as button } from '@src/components/shared/MainButton';
 import { theme } from '@src/assets/styles';
 
-export const Container = styled.div`
+export const Root = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  display: flex;
-  width: 100%;
-  height: 100%;
-  background: none;
-`;
-
-export const ModalWrapper = styled.div`
-  display: flex;
   width: 100%;
   max-width: 700px;
   height: 100%;
+  display: flex;
   margin: 0 auto;
   padding: 0 20px;
   background-color: ${(props) => rgba(props.theme.colors.basic.gray00, 0.75)};
